536852d958dc6bc13e1d6d8887e45d62d6565af6
xbcnvbcnvccxcv <wjddbwl3116@naver.com>
2024-07-02 17:46:13 +0900

    과제
    환율계산, 회원가입

---
+++ 과제.py
@@ -0,0 +1,4 @@
+exchange = {'달러': 1320, '엔화': 800, '위안': 130}
+chul = [13, 200, 13]
+
+print(exchange['달러']* chul[0] + exchange['엔화']+chul[1])
---
+++ 환율계산.py
@@ -0,0 +1,5 @@
+exchange = {'달러': 1320, '엔화': 800, '위안': 130}
+chul = [13, 200, 13]
+
+total = exchange['달러']* chul[0] + exchange['엔화']+chul[1]
+print('가지고 계신 돈은', total, '원 입니다.')
